4426de4dd5886930e51af7cbed00016966e24678
“postyizhan” <185839426@qq.com>
2024-08-08 18:34:26 +0800

    修改主页

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -2,5 +2,5 @@ import type { DefaultTheme } from 'vitepress'
 
 export const nav: DefaultTheme.Config['nav'] = [
   { text: '驿站导航', link: '/nav/' },
-  { text: '驿站主页', link: 'https://yizhan.wiki' },
+  { text: '驿站主页', link: '/home.md' },
 ]
